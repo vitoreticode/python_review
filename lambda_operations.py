5d614c541531b6117ac7c3ad85f3b8db7021d5a4
U-SERASA\c82414a <c82414a@NB65862.serasa.intranet>
2020-09-10 10:34:29 -0300

    update list comprehension

--- lambda_operations.py
+++ lambda_operations.py
@@ -5,3 +5,11 @@ def function_with_callback(num, callback):
     return callback(num)
 
 print(function_with_callback(2, squared))
+
+odd_or_even = lambda num: 'even'if num % 2 == 0 else 'odd'
+odd_or_even(3)
+
+# Returns a dictionary counting charaters in a string
+lambda2 = lambda s: dict([(c, s.count(c)) for c in set(s)])
+print(func2('DataCamp'))
+print(lambda2('DataCamp'))
